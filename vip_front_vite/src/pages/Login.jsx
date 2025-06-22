2f48e18a99997a571ad41bbb11b34a6d21e681a0
wevertonvip010 <wevertondlima@gmail.com>
2025-06-21 23:54:42 -0300

    Add files via upload

---
+++ vip_front_vite/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bem-vindo ao sistema VIP Mudanças</h1>
+      <p>Login funcional carregado com sucesso!</p>
+    </div>
+  );
+}
+
+export default Login;
