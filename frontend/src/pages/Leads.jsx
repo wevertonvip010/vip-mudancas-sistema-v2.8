f053cd8a00bdf55123daa2c4a8e86a3e3f81dd56
wevertonvip010 <wevertondlima@gmail.com>
2025-06-22 10:04:22 -0300

    Create Leads.jsx

---
+++ frontend/src/pages/Leads.jsx
@@ -0,0 +1,8 @@
+export default function Leads() {
+  return (
+    <div className="p-4">
+      <h1>Leads Capturados</h1>
+      <p>Aqui serão listados os leads do LinkedIn e formulário.</p>
+    </div>
+  );
+}
