980ce31cb439a7a36f26b700b546b9eedc6ab0e6
wevertonvip010 <wevertondlima@gmail.com>
2025-06-22 10:10:05 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
+import Leads from './pages/Leads';
+import Licitacoes from './pages/Licitacoes';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <>
-      <h1>VIP Mudanças v2.8</h1>
-      <p>Seu sistema rodando com sucesso!</p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Página inicial: Login */}
+        <Route path="/" element={<Login />} />
+
+        {/* Páginas internas com Sidebar */}
+        <Route path="/dashboard" element={<Sidebar><Dashboard /></Sidebar>} />
+        <Route path="/leads" element={<Sidebar><Leads /></Sidebar>} />
+        <Route path="/licitacoes" element={<Sidebar><Licitacoes /></Sidebar>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
